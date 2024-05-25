bf99a8f336a1ce53debed1b56d4672b4d5049ce6
viniciusfal <vinicius_prsilva@hotmail.com>
2024-05-25 16:10:22 -0300

    feat: logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/viniciusfal/go-opportunities/router"
+import (
+	"github.com/viniciusfal/go-opportunities/config"
+	"github.com/viniciusfal/go-opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
